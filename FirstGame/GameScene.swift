751a942e901d7248d2e0374e8a0c58f0d871edc4
kolesnikovakate <KolesnikovA123>
2014-11-06 10:45:29 +0400

    Первые приготовления

---
+++ FirstGame/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  FirstGame
+//
+//  Created by Екатерина Колесникова on 06.11.14.
+//  Copyright (c) 2014 KKate. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ FirstGame/GameViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GameViewController.swift
+//  FirstGame
+//
+//  Created by Екатерина Колесникова on 06.11.14.
+//  Copyright (c) 2014 KKate. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
