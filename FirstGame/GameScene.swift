cdf6ad452fc345c1612520790b6b9c4ec11e0d72
kolesnikovakate <KolesnikovA123>
2014-11-06 11:18:28 +0400

    Update GameScene

--- FirstGame/GameScene.swift
+++ FirstGame/GameScene.swift
@@ -8,11 +8,13 @@
 
 import SpriteKit
 
+let TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+
+    var tick:(() -> ())?
+    var tickLengthMillis = TickLengthLevelOne
+    var lastTick:NSDate?
 
     required init(coder aDecoder: NSCoder) {
         fatalError("NSCoder not supported")
@@ -27,5 +29,24 @@ class GameScene: SKScene {
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
         addChild(background)
+    }
+
+    override func update(currentTime: CFTimeInterval) {
+        if lastTick == nil {
+            return
+        }
+        var timePassed = lastTick!.timeIntervalSinceNow * -1000.0
+        if timePassed > tickLengthMillis {
+            lastTick = NSDate()
+            tick?()
+        }
+    }
+
+    func startTicking() {
+        lastTick = NSDate()
+    }
+
+    func stopTicking() {
+        lastTick = nil
     }
 }
